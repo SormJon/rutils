db1e7dfeebb67a1145226ebff09aa7db7ba0df1d
Silvio Clecio <silvioprog@gmail.com>
2012-11-05 20:13:18 -0200

    # Cleanup.

--- src/rutils.pas
+++ src/rutils.pas
@@ -2,12 +2,7 @@
   RUtils plugin.
   Copyright (C) 2012-2014 Silvio Clecio.
 
-  AUTHORS:
-  Joao Morais, Luciano Souza.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  Please see the LICENSE file.
 *)
 
 unit RUtils;
